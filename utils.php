5e3d247cc58bdb093898f7edcf97b780d85ae8c7
quangvh62 <38472958+quangvh62wru@users.noreply.github.com>
2018-12-18 06:01:00 -0800

    Initial commit

---
+++ utils.php
@@ -0,0 +1,109 @@
+<?php
+class Utils{
+	function getToken($length=32){
+		$token = "";
+		$codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		$codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+		$codeAlphabet.= "0123456789";
+		for($i=0;$i<$length;$i++){
+			$token .= $codeAlphabet[$this->crypto_rand_secure(0,strlen($codeAlphabet))];
+		}
+		return $token;
+	}
+
+	function crypto_rand_secure($min, $max) {
+		$range = $max - $min;
+    if ($range < 0) return $min; // not so random...
+    $log = log($range, 2);
+    $bytes = (int) ($log / 8) + 1; // length in bytes
+    $bits = (int) $log + 1; // length in bits
+    $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+    do {
+    	$rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+        $rnd = $rnd & $filter; // discard irrelevant bits
+    } while ($rnd >= $range);
+    return $min + $rnd;
+}
+// 11.4 Change create() method
+
+// Since we added a new field, we need to change the create() method in the User class.
+
+// Open objects/user.php file. Replace the create() method with the new one below.
+// create new user record
+function create(){
+
+    // to get time stamp for 'created' field
+	$this->created=date('Y-m-d H:i:s');
+
+    // insert query
+	$query = "INSERT INTO " . $this->table_name . "
+	SET
+	firstname = :firstname,
+	lastname = :lastname,
+	email = :email,
+	contact_number = :contact_number,
+	address = :address,
+	password = :password,
+	access_level = :access_level,
+	access_code = :access_code,
+	status = :status,
+	created = :created";
+
+    // prepare the query
+	$stmt = $this->conn->prepare($query);
+
+    // sanitize
+	$this->firstname=htmlspecialchars(strip_tags($this->firstname));
+	$this->lastname=htmlspecialchars(strip_tags($this->lastname));
+	$this->email=htmlspecialchars(strip_tags($this->email));
+	$this->contact_number=htmlspecialchars(strip_tags($this->contact_number));
+	$this->address=htmlspecialchars(strip_tags($this->address));
+	$this->password=htmlspecialchars(strip_tags($this->password));
+	$this->access_level=htmlspecialchars(strip_tags($this->access_level));
+	$this->access_code=htmlspecialchars(strip_tags($this->access_code));
+	$this->status=htmlspecialchars(strip_tags($this->status));
+
+    // bind the values
+	$stmt->bindParam(':firstname', $this->firstname);
+	$stmt->bindParam(':lastname', $this->lastname);
+	$stmt->bindParam(':email', $this->email);
+	$stmt->bindParam(':contact_number', $this->contact_number);
+	$stmt->bindParam(':address', $this->address);
+
+    // hash the password before saving to database
+	$password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+	$stmt->bindParam(':password', $password_hash);
+
+	$stmt->bindParam(':access_level', $this->access_level);
+	$stmt->bindParam(':access_code', $this->access_code);
+	$stmt->bindParam(':status', $this->status);
+	$stmt->bindParam(':created', $this->created);
+
+    // execute the query, also check if query was successful
+	if($stmt->execute()){
+		return true;
+	}else{
+		$this->showError($stmt);
+		return false;
+	}
+
+	// send email using built in php mailer
+	public function sendEmailViaPhpMail($send_to_email, $subject, $body){
+
+		$from_name="VoteWeb";
+		$from_email="quangzetsu@gmail.com";
+
+		$headers  = "MIME-Version: 1.0\r\n";
+		$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+		$headers .= "From: {$from_name} <{$from_email}> \n";
+
+		if(mail($send_to_email, $subject, $body, $headers)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+}
+}
+?>
